51a13cf9f48ab8643c1f1c4fda5b1c1a44469bb5
priyanshusingh305 <singhpriyanshu305@gmail.com>
2021-09-18 03:31:22 +0530

    first

---
+++ DAY5/index.js
@@ -0,0 +1,7 @@
+// var fs = require ("fs");
+// fs.mkdirSync("DAY6");
+// fs.writeFileSync("DAY6/HELLO.TXT","HELLO");
+const Test = require("./require_test");
+var obj = new Test();
+var returned_val =obj.print();
+console.log(returned_val);
---
+++ DAY5/require_test.js
@@ -0,0 +1,12 @@
+class Test {
+    print(){
+        console.log("i am inside required test");
+    }
+}
+class Test2 {
+    print(){
+        console.log("t");
+    }
+}
+
+module.exports = Test;
