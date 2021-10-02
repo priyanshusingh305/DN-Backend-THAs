7e158b3da23f3c5a01b42635ab14123ddb5b5aa2
priyanshusingh305 <singhpriyanshu305@gmail.com>
2021-10-02 23:11:11 +0530

    ⭐

--- DAY09/index.js
+++ DAY09/index.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const app = express()
-const port = 5000
+const path = require("path")
+const port = 9000
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "jade");
+console.log(__dirname);
+app.use("/",(req,res)=>{
+    // res.download(path.join(__dirname,"Public/text.json"),'test.json')
+    // res.download(path.join(__dirname,"Public/levi.jpeg"),'levi.jpeg')
+    // res.sendFile(path.join(__dirname,"Public/levi.jpeg"),'levi.jpeg')
+    // res.sendFile(path.join(__dirname,"Public/text.json"),'test.txt')
+    // res.sendFile(path.join(__dirname,"Public/levi.jpeg"),'levi.jpeg')
+    //   res.render('index',{title:"Express"})
+    res
+    .status(201)
+    .cookie("token","test",{
+       expire : new Date(Date.now() + 8*3600000)
+    })
+    .cookie("hello","hello");
+    // .redirect(301,"/admin")
+})
 
-
-app.listen(port, () => console.log("Listening at 5000"))
+app.listen(port);
